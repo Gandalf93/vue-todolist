44ac0831bf0441ba8d00eb2c84c3dafe7456a147
Giulia <giuliatullia93@gmail.com>
2021-06-25 00:25:05 +0200

    add-array-stampato

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@
 // - stampare in pagina un item per ogni elemento contenuto in un array
 // - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
 // - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+
+
+const ap = new Vue (
+    {
+        el : '#app',
+        data :{
+            lista: [
+                'fare la spesa',
+                'lavare i piatti',
+                'dar da mangiare ai gatti',
+                'studiare',
+                'andare al cinema'
+            ]
+        }
+
+    }
+);
