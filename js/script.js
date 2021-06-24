02a1ee8552e9e279f497c93c9dac51a799f3260c
Giulia <giuliatullia93@gmail.com>
2021-06-25 00:35:12 +0200

    add-input-new-item

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@
 // - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
 
 
-const ap = new Vue (
+const app = new Vue (
     {
         el : '#app',
         data :{
+            newli : '',
             lista: [
                 'fare la spesa',
                 'lavare i piatti',
@@ -15,7 +16,12 @@ const ap = new Vue (
                 'studiare',
                 'andare al cinema'
             ]
+        },
+        methods:{
+            addElemento (){
+                this.lista.push(this.newli);
+                this.newli = ''
+            }
         }
-
     }
 );
